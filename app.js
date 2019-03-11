631ba40960a20d63379a439fe024bbfcb059ae11
cesarRuelas <cesar.ruagui@gmail.com>
2019-03-10 18:30:16 -0700

    this commit has the first list displayed correctly

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
-var app= angular.module("module2App", []);
-app.controller("Controller2", [$scope, $filter, Controller2]);
-function Controller2($scope, $filter){
+    var app=angular.module("module2App", []);
+    app.controller("Controller2", ['$scope', '$filter',Controller]);
+    function Controller($scope, $filter){
 $scope.shoppingList=[
     {Name:"Chocolates", Number:"10"},
     {Name:"Cereal", Number:"2"},
@@ -9,6 +9,8 @@ $scope.shoppingList=[
     {Name:"Bread", Number:"1"},
     {Name:"Apples", Number:"5"},
     {Name:"Chicken", Number:"1"}];
+$scope.shoppingList2=["chocolate","flour", "pizza"];
+$scope.something="its working";
 
 
 };
