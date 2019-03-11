f62147f30f5409add40527604ecb53ab31457097
cesarRuelas <cesar.ruagui@gmail.com>
2019-03-10 19:37:59 -0700

    this commit is working how it is supposed to do. But using 1 controller

--- app.js
+++ app.js
@@ -9,8 +9,34 @@ $scope.shoppingList=[
     {Name:"Bread", Number:"1"},
     {Name:"Apples", Number:"5"},
     {Name:"Chicken", Number:"1"}];
-$scope.shoppingList2=["chocolate","flour", "pizza"];
-$scope.something="its working";
+$scope.empty = false;
+$scope.empty2=true;
+$scope.message=false;
+$scope.alreadybought=[];
+$scope.validate= function(){
+    if($scope.shoppingList.length>0)
+    {
+        $scope.empty=false;
+    }
+    else
+    {
+        $scope.empty=true;
+    }
+    if($scope.alreadybought.length>0)
+    {
+        $scope.empty2=false;
+    }
+    else
+    {
+        $scope.empty2=true;
+    }
+};
+$scope.moveItem= function(index){
+    var number=$scope.alreadybought.length;
+    $scope.alreadybought[number]=$scope.shoppingList[index];
+    $scope.shoppingList.splice(index, 1);
+    $scope.validate();
+};
 
 
 };
