35028a307cd3c4ce6409dd9b103b32707f40a19c
cesarRuelas <cesar.ruagui@gmail.com>
2019-03-10 17:36:19 -0700

    commit with first list displayed

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 var app= angular.module("module2App", []);
 app.controller("Controller2", [$scope, $filter, Controller2]);
 function Controller2($scope, $filter){
-$scope.shoppingList=[];
+$scope.shoppingList=[
+    {Name:"Chocolates", Number:"10"},
+    {Name:"Cereal", Number:"2"},
+    {Name:"Tuna", Number:"5"},
+    {Name:"Bread", Number:"1"},
+    {Name:"Apples", Number:"5"},
+    {Name:"Chicken", Number:"1"}];
+
 
 };
 })();
